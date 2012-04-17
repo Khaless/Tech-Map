5abb006eccc14359591a8b0a110128138c720611
Mathew Rodley <mathew@rodley.com.au>
2012-04-18 00:33:10 +1000

    Added support for browsers that do not support/do not want to support HTML GeoLocation.

--- public/javascripts/tagin.js
+++ public/javascripts/tagin.js
@@ -1,3 +1,4 @@
+
 $(document).ready(
 		function() {
 
@@ -33,9 +34,40 @@ $(document).ready(
 					return results;
 				}
 			});
-
+			
+			
+			var geocoder = new google.maps.Geocoder();
+			
+			var notSupportedFn = function() {
+				
+				if(notSupportedTimer) {
+					clearTimeout(notSupportedTimer);
+				}
+				
+				$('#tag-location-span').text('Sorry, we could not determine your location. Please type it in below...');
+				$('#tag-location-alt').show();
+				$('#tag-location-alt-btn').click(function() {
+				    geocoder.geocode( { 'address': $('#tag-location-alt-input').attr('value') }, function(results, status) {
+				        if (status == google.maps.GeocoderStatus.OK) {
+				        	var str = results[0].formatted_address;
+				        	var lat = results[0].geometry.location.lat(); 
+				        	var lng = results[0].geometry.location.lng();
+				        	$('#tag-location').attr("value", lat + "," + lng);
+				        	$('#tag-location-span').html("<b>Address Found:</b> " + str + " [" + lat + "," + lng + "]");
+				          } else {
+				        	$('#tag-location-span').text('Could not find address. Please try again.');
+				          }
+				    });
+				    return false;
+				});
+			}
+			
+			var notSupportedTimer = setTimeout(notSupportedFn, 5000);
+			
 			$.geolocation.find(function(loc) {
-
+				
+				clearTimeout(notSupportedTimer);
+				
 				var location = {
 					'lat' : loc.latitude,
 					'lng' : loc.longitude,
@@ -49,7 +81,6 @@ $(document).ready(
 				 * Attempt to use Google Geocoding services to find Country,
 				 * Administrative Area and Locality.
 				 */
-			    var geocoder = new google.maps.Geocoder();
 			    var latlng = new google.maps.LatLng(loc.latitude, loc.longitude);
 			    geocoder.geocode( { 'latLng': latlng}, function(results, status) {
 			        if (status == google.maps.GeocoderStatus.OK) {
@@ -59,6 +90,5 @@ $(document).ready(
 			        	$('#tag-location-span').text(location.str);
 			          }
 			    });
-			});
-
+			}, notSupportedFn);
 		});
